971188ebb8c95c21d2457a14be56be4c628c60dc
zjingqi <zjingqi154@qq.com>
2019-03-31 17:52:57 +0800

    test

---
+++ freecodecamp/README.md
@@ -0,0 +1,4 @@
+﻿# FrontEndStudy
+记录下前端学习的过程
+zjingqi
+jfasdklkjfd786453
---
+++ freecodecamp/Test/jax.js
@@ -0,0 +1,58 @@
+
+
+
+// function train(result){
+//     console.log(result);
+// }
+
+
+
+// document.getElementById('button2').addEventListener('click',loadUser);
+// function loadUser(){
+//     var xhr =new XMLHttpRequest();
+//     xhr.open("GET","jax.json",true);
+//     xhr.onload=function(){
+//         if(this.status==200){
+
+//             function train(result){
+//                // console.log(result);
+//                 var html='ul';
+//                 for(var i=0;i<result.length;i++){
+//                     console.log(result[i]);
+//                     // try{
+//                     //      var json1=JSON.parse(result[i]);
+//                     // }catch(e){
+//                     //     var json1=eval("("+result[i]+")");
+//                     // }
+//                     // html+='<li>'+json1+'</li>';
+//                 }                
+//                 html+='<ul>';
+//                 document.getElementById('user2').innerHTML=html;
+//             }
+            
+//             // var user=JSON.parse(this.responseText);
+//             // console.log(user);
+//             // var output='';
+//             // for(var i in user){
+//             //     output+=
+//             //         '<ul>'+
+//             //             '<li>'+user[i].id+'</li>'+
+//             //             '<li>'+user[i].province+'</li>'+
+//             //             '</ul>';         
+//             // }
+//             // document.getElementById('user2').innerHTML=output;
+//             // console.log(this.responseText);
+//         }
+//     }
+//     xhr.send();
+// }
+
+
+
+
+
+
+
+
+
+
